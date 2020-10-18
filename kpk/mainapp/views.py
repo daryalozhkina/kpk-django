876c2c8acb994774291025cfc7f34ed7201cc1bb
ь <das.lozh19@mail.ru>
2020-10-18 19:44:44 +0500

    django

---
+++ kpk/mainapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'mainapp/index.html')
+
+def catalog(request):
+    pass
+
+def basket(request):
+    pass
+# Create your views here.
